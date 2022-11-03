2472259b396b139e6bbbc43a276813eb45869598
Eric J <51121192+EricJ-code@users.noreply.github.com>
2022-11-03 11:36:38 -0400

    Update TrainOfGears.py

--- TrainOfGears.py
+++ TrainOfGears.py
@@ -1,9 +1,26 @@
 
-
-import gear
+from gear import Gear
 class trainOfGears:
-    def __init__(self, gear):
-        self.gear = gear
-        self.left = None
-        self.right = None
+    def __init__(self):
+        self.gear = Gear()
+        self.left = Gear()
+        self.right = Gear()
+    def makeTrain(self,gear):
+        self.gear.name("Parent")
+        self.left.nameGear("E")
+        self.right.nameGear("E2")
+
+        chars = [x for x in gear]
+        self.gear.addTooth(chars)
         
+        for i in range(self.gear.length()):
+            self.left.addTooth(1)
+            self.right.addTooth(1)
+    def pRotL(self):
+        self.gear.rotL()
+        self.left.rotR()
+        self.right.rotR()
+    def pRotR(self):
+        self.gear.rotR()
+        self.left.rotL()
+        self.right.rotL()
