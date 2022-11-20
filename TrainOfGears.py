6b4e42c85d0eae0ef0ef4aa86fcbbc6832703cc1
CarllarC <90017476+CarllarC@users.noreply.github.com>
2022-11-19 19:27:57 -0500

    Update TrainOfGears.py
    New and Improved Encode and Decode....not sure if I used the Correct Functions(pRotR vs pRotL) but I think the encoding and decoding works as discussed

--- TrainOfGears.py
+++ TrainOfGears.py
@@ -33,22 +33,49 @@ class trainOfGears:
         self.gear.rotR()
         self.left.rotL()
         self.right.rotL()
-
-    def encode(self): #still need to make sure this is turning correctly, sorry oops
+    '''The following functions currently 
+        print everything they do so that we can
+        all follow along, please run this file'''
+    def encode(self):
         y = self.gear.length()
         print(y)
         for i in range(y - 1):
             self.pRotL()
-            self.right.append(self.gear.pop(1))
-            self.gear.insert(0, self.right.pop(-2))
-            self.left.insert(0, self.gear.pop(-1))
-            self.gear.append(self.left.pop(-1))
-        # self.left.insert(0, self.gear.pop(-1))
-        # self.gear.insert(0,self.right.pop(-2))
-        self.gear.printGear()
-        self.right.printGear()
-        self.left.printGear()
+            x = self.gear.pop(0)
+            y = self.right.pop(-1)
+            self.right.append(x)
+            self.gear.insert(0, y)
+            x = self.gear.pop(-1)
+            y = self.left.pop(0)
+            self.left.insert(0, x)
+            self.gear.append(y)
+            self.gear.printGear()
+            self.right.printGear()
+            self.left.printGear()
+
+    def decode(self):
+        y = self.gear.length()
+        print(y)
+        for i in range(y - 1):
+
+            x = self.gear.pop(0)
+            y = self.right.pop(-1)
+            self.right.append(x)
+            self.gear.insert(0, y)
+            x = self.gear.pop(-1)
+            y = self.left.pop(0)
+            self.left.insert(0, x)
+            self.gear.append(y)
+            self.pRotR()
+            self.gear.printGear()
+            self.right.printGear()
+            self.left.printGear()
+
+
+
 
 ToG = trainOfGears()
 ToG.makeTrain("Gear")
 ToG.encode()
+ToG.decode()
+
